4a54fb367d5bdac31edc9e635740d7f7d278ea6b
Thyago Freitas da Silva <thyago_freitas@virgo.atlantico.net.br>
2021-06-21 13:50:46 -0300

    subindo poc utilizando shedlock e spring batch

--- shedlock/src/main/java/dell/com/shedlock/job/SomeScheduledTask.java
+++ shedlock/src/main/java/dell/com/shedlock/job/SomeScheduledTask.java
@@ -33,21 +33,10 @@ public class SomeScheduledTask {
     }
 
     @Scheduled(cron = "0 0/1 * * * ?")
-    @SchedulerLock(
-            // Identificador da task
-            name = "TaskScheduler_scheduledTask",
-            // O bloqueio ira ser por 2 minutos e meio ,150 segundos, ou seja,mesmo que o job termine sua execução mais rapido que 2 minutos e meio, nenhuma task com mesmo identificador
-            // poderar executar dentro dessa janela de tempo .
-            lockAtLeastFor = "PT150S",
-            // Apos 10 minutos de inicio de execucao, caso ainda não tenha terminado, outra instancia pode executar a task, pois o lock será desfeito.
-            // Tomar cuidado pois pode criar inconsistência nos dados.
-            // Se esse valor não for passado, como DEFAULT será utilizado o valor definido na annotation @EnableSchedulerLock utilizado na Main class.
-            lockAtMostFor = "PT10M"
-    )
+    @SchedulerLock(name = "task_id", lockAtLeastFor = "PT150S", lockAtMostFor = "PT10M")
     public void run() throws Exception{
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String message = String.format("Executando instancia : %s no momento %s",this.instanceIdentifier,LocalDateTime.now().format(formatter));
-        logger.info(message);
+        logger.info("Running instance : {} at {}",this.instanceIdentifier,LocalDateTime.now().format(formatter));
         jobLauncher.run(job, new JobParametersBuilder().addDate("date", new Date()).toJobParameters());
     }
 }
